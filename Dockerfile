c7fa7768953703ab5993dcd9ec3f509122f40ca4
Rishikumar <jaisriandugala207@.com>
2024-12-02 22:49:21 +0530

    my first commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM amazoncorretto:17
+
+WORKDIR /app
+
+COPY target/pavan-app.jar /app/pavan-app-1.jar
+
+EXPOSE 8080:8000
+
+ENTRYPOINT [ "java" "-jar" "pavan-app-1.jar"]
---
+++ src/main/java/com/sri/controller/Mycontroller.java
@@ -0,0 +1,14 @@
+package com.sri.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Mycontroller {
+	@GetMapping("/welcome")
+	public String welcome()
+	{
+		return "Welcome Mr Akash";
+	}
+
+}
